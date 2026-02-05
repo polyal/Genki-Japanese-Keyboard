41262549cb42c378819489db13008309e1c8a178
pwner <lorant.polya@gmail.com>
2026-02-05 11:05:44 -0500

    traverse and build individual hiragana

--- keyboard/src/main.rs
+++ keyboard/src/main.rs
@@ -12,13 +12,15 @@ struct Kana {
     next: Vec<Kana>,
 }
 
-fn iterate_kana<F>(head: &Kana, f: &mut F)
-  where F: FnMut(Option<char>, Option<char>),
+fn iterate_kana<F>(head: &Kana, cb: &mut F)
+where 
+  F: FnMut(&Kana, bool), 
   {
-    f(head.value, head.ext);
+    cb(&head, true);
     for child in &head.next {
-        iterate_kana(child, f);
+        iterate_kana(child, cb);
     }
+    cb(&head, false);
 }
 
 fn main() {
@@ -32,19 +34,32 @@ fn main() {
       .expect("couldnt read kana/hiragana.json");
 
     let mut single_hiragana = String::new();
+    let mut chars_pushed: Vec<u8> = Vec::new();
 
-    let mut string_pusher = move |value: Option<char>, ext: Option<char>| {
-      if let Some(value) = value {
-        single_hiragana.push(value);
-        if let Some(ext) = ext {
-          single_hiragana.push(ext);
+    let mut hiragana_creator = |node: &Kana, pre: bool| {
+      if pre {
+        let mut num_pushed: u8 = 0;
+        if let Some(value) = node.value {
+          single_hiragana.push(value);
+          num_pushed += 1;
+          if let Some(ext) = node.ext {
+            single_hiragana.push(ext);
+            num_pushed += 1;
+          }
+          println!("{single_hiragana}");
+        }
+        chars_pushed.push(num_pushed);
+      }
+      else {
+        let num_pushed = chars_pushed.pop();
+        for _i in 1..=num_pushed.unwrap() {
+          single_hiragana.pop();
         }
       }
-      println!("{single_hiragana}");
     };
 
     let head = serde_json::from_str::<Kana>(&json).unwrap();
     
     // dbg!(&result);
-    iterate_kana(&head, &mut string_pusher);
+    iterate_kana(&head, &mut hiragana_creator);
 }
