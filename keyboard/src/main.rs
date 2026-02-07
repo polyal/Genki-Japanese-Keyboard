cb8426d247d6a97cf86cfaff05ce6ceed5da62dd
pwner <lorant.polya@gmail.com>
2026-02-06 23:23:07 -0500

    small cleanup

--- keyboard/src/main.rs
+++ keyboard/src/main.rs
@@ -15,14 +15,12 @@ fn main() {
   println!("converted '{buffer}' -> '{kana}'");
 
   let book = Book::new();
-
   for lesson in &book.lessons.lessons {
     println!("lesson_{}: {}/{}", lesson.index, lesson.name_en, lesson.name_jp);
-    println!("lesson_{}: {}/{}", lesson.index, lesson.name_en, lesson.name_jp);
     for vocab in &lesson.vocab {
-      println!("  section{}", vocab.name);
+      println!("  section: {}", vocab.name);
       for phrase in &vocab.phrases {
-        println!("    {} - {}", phrase.en, phrase.jp);
+        println!("    '{}' - '{}'", phrase.en, phrase.jp);
       }
     }
   }
