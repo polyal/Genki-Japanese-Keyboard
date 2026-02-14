28cd462e548a39b539f81662e742b34856b58f47
pwner <lorant.polya@gmail.com>
2026-02-14 14:58:54 -0500

    add tests for lesson 3 kanji

--- keyboard/src/kanji_converter.rs
+++ keyboard/src/kanji_converter.rs
@@ -108,3 +108,60 @@ impl HiragaToKanjiConverter {
     return phrase.get_kanji();
   }
 }
+
+
+#[cfg(test)]
+mod tests {
+  // Note this useful idiom: importing names from outer (for mod tests) scope.
+  use super::*;
+
+  #[test]
+  fn test_converter() {
+    let converter = HiragaToKanjiConverter::new();
+    let test_cases = vec![
+      ("いち", vec!['一']),
+      ("いっ", vec!['一']),
+      ("ひと", vec!['一']),
+      ("に", vec!['二']),
+      ("ふた", vec!['二']),
+      ("さん", vec!['三']),
+      ("みっ", vec!['三']),
+      ("し", vec!['四']),
+      ("よん", vec!['四']),
+      ("よ", vec!['四']),
+      ("よっ", vec!['四']),
+      ("ご", vec!['五']),
+      ("いつ", vec!['五']),
+      ("ろく", vec!['六']),
+      ("ろっ", vec!['六']),
+      ("むっ", vec!['六']),
+      ("しち", vec!['七']),
+      ("なな", vec!['七']),
+      ("はち", vec!['八']),
+      ("はっ", vec!['八']),
+      ("やっ", vec!['八']),
+      ("きゅう", vec!['九']),
+      ("く", vec!['九']),
+      ("ここの", vec!['九']),
+      ("じゅう", vec!['十']),
+      ("じゅっ", vec!['十']),
+      ("じっ", vec!['十']),
+      ("とお", vec!['十']),
+      ("ひゃく", vec!['百']),
+      ("びゃく", vec!['百']),
+      ("ぴゃく", vec!['百']),
+      ("せん", vec!['千']),
+      ("ぜん", vec!['千']),
+      ("まん", vec!['万']),
+      ("えん", vec!['円']),
+      ("まる", vec!['円']),
+      ("じ", vec!['時']),
+      ("とき", vec!['時']),
+    ];
+
+    for (romaji, expected) in test_cases {
+      let result = converter.convert(&romaji.to_string());
+      assert_eq!(result, expected, "Failed for romaji: {}", romaji);
+    }
+  }
+}
